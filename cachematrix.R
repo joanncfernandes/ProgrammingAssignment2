1f0e7d077eb1c94b3e6b2025aa465c314096795c
git config --global user.name Joanngit config --global user.name <joanncfernandes@gmail.com>
2014-07-27 21:52:15 +0530

    comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two functions creates a matrix and computes and caches
+##the inverse of th matrix.
+##We only input an invertible matrix 
 
-## Write a short comment describing this function
+## The first functionmakeCacheMatrix creates a special "matrix"
+##object that can cache its inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## This second function caches the value of the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
